a4eeaf108cb182a74407fc37129d19bdc9220583
Cycloneblaze <accounts@cycloneblaze.net>
2021-05-18 16:48:56 +0200

    Change ownership to use classes, not IDs

--- GPX+.js
+++ GPX+.js
@@ -1,27 +1,32 @@
 function mark(elem) {
-    let idStr = elem.id;
-    console.log(elem, elem.title, idStr);
-    if (idStr === "own") {
-        elem.id = "notown";
-    } else if (idStr === "notown") {
-        elem.id = "own";
+    if (elem.classList.contains("own")) {
+        elem.classList = "notown";
+    } else if (elem.classList.contains("notown")) {
+        elem.classList = "own";
     }
-    localStorage.setItem(elem.title, elem.id);
+    /* note that these assume that the Pokémon images only have EITHER
+    the class "own" OR the class "notown", since they overwrite the
+    whole class list with only one of those two.*/
+    localStorage.setItem("shiny ".concat(elem.title), elem.classList);
 }
 
 function markAll() {
     const elems = document.getElementsByTagName("img");
-    /* a class on each img would be necessary if there were any imgs that were not Pokémon,
-    but there are not */
+    /* an extra class on each img would be necessary if there were any imgs that were
+    not Pokémon, but there are not */
     
     for (let i = 0; i < elems.length; i++) {
+        if (elems[i].id === "Top") {
+            continue;
+        }
         elems[i].onclick = mark; // to ensure this is the caller
         elems[i].addEventListener('click', function(){
             mark(this); // 'this' is <img ...> (the clicked element) in this context
         });
 
-        let state = localStorage.getItem(elems[i].title);
+        let state = localStorage.getItem("shiny ".concat(elems[i].title));
         if (state !== null) {
+            console.log('state is not "", state is', state);
             if (state !== elems[i].title) {
                 mark(elems[i]);
             }
