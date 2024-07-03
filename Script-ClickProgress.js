5d1c86b5ad7e38da4a7f24b2c68352661420d0f4
Level-X <84361339+Level-X@users.noreply.github.com>
2024-07-02 21:14:38 -0500

    Updated for v4.0
    Reworked code to be compatible with the new layout. Added new comments.

--- Script-ClickProgress.js
+++ Script-ClickProgress.js
@@ -3,7 +3,7 @@ function countProgress(generation) {
     console.log("generation was", generation)
     switch (generation) {
         case 'Gen1':
-            let elemsGen1 = document.querySelectorAll('.Gen1 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen1 = document.querySelectorAll('.Gen1 > img:not([src="Images/Pokémon/0.png"])');
             /* More advanced usage of querySelectorAll! Who needs getElementsByClassName? This selects all
             elements which have the class .Gen9, and also have at least one direct child element which has
             the attribute src=the placeholder image link. We need to do this because we need to only select
@@ -25,7 +25,7 @@ function countProgress(generation) {
             ensures the progress bars are correct. */
             break;
         case 'Gen2':
-            let elemsGen2 = document.querySelectorAll('.Gen2 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen2 = document.querySelectorAll('.Gen2 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen2 = Array.from(elemsGen2).filter(value => Array.from(document.querySelectorAll('.Gen2 .own')).includes(value));
             let progGen2 = document.getElementById('progressBars').getElementsByTagName('progress')[1];
             progGen2.setAttribute("value", ownedGen2.length);
@@ -33,7 +33,7 @@ function countProgress(generation) {
             progGen2.setAttribute("max",   elemsGen2.length);
             break;
         case 'Gen3':
-            let elemsGen3 = document.querySelectorAll('.Gen3 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen3 = document.querySelectorAll('.Gen3 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen3 = Array.from(elemsGen3).filter(value => Array.from(document.querySelectorAll('.Gen3 .own')).includes(value));
             let progGen3 = document.getElementById('progressBars').getElementsByTagName('progress')[2];
             progGen3.setAttribute("value", ownedGen3.length);
@@ -41,7 +41,7 @@ function countProgress(generation) {
             progGen3.setAttribute("max",   elemsGen3.length);
             break;
         case 'Gen4':
-            let elemsGen4 = document.querySelectorAll('.Gen4 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen4 = document.querySelectorAll('.Gen4 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen4 = Array.from(elemsGen4).filter(value => Array.from(document.querySelectorAll('.Gen4 .own')).includes(value));
             let progGen4 = document.getElementById('progressBars').getElementsByTagName('progress')[3];
             progGen4.setAttribute("value", ownedGen4.length);
@@ -49,7 +49,7 @@ function countProgress(generation) {
             progGen4.setAttribute("max",   elemsGen4.length);
             break;
         case 'Gen5':
-            let elemsGen5 = document.querySelectorAll('.Gen5 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen5 = document.querySelectorAll('.Gen5 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen5 = Array.from(elemsGen5).filter(value => Array.from(document.querySelectorAll('.Gen5 .own')).includes(value));
             let progGen5 = document.getElementById('progressBars').getElementsByTagName('progress')[4];
             progGen5.setAttribute("value", ownedGen5.length);
@@ -57,7 +57,7 @@ function countProgress(generation) {
             progGen5.setAttribute("max",   elemsGen5.length);
             break;
         case 'Gen6':
-            let elemsGen6 = document.querySelectorAll('.Gen6 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen6 = document.querySelectorAll('.Gen6 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen6 = Array.from(elemsGen6).filter(value => Array.from(document.querySelectorAll('.Gen6 .own')).includes(value));
             let progGen6 = document.getElementById('progressBars').getElementsByTagName('progress')[5];
             progGen6.setAttribute("value", ownedGen6.length);
@@ -65,7 +65,7 @@ function countProgress(generation) {
             progGen6.setAttribute("max",   elemsGen6.length);
             break;
         case 'Gen7':
-            let elemsGen7 = document.querySelectorAll('.Gen7 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen7 = document.querySelectorAll('.Gen7 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen7 = Array.from(elemsGen7).filter(value => Array.from(document.querySelectorAll('.Gen7 .own')).includes(value));
             let progGen7 = document.getElementById('progressBars').getElementsByTagName('progress')[6];
             progGen7.setAttribute("value", ownedGen7.length);
@@ -73,7 +73,7 @@ function countProgress(generation) {
             progGen7.setAttribute("max",   elemsGen7.length);
             break;
         case 'Gen8':
-            let elemsGen8 = document.querySelectorAll('.Gen8 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen8 = document.querySelectorAll('.Gen8 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen8 = Array.from(elemsGen8).filter(value => Array.from(document.querySelectorAll('.Gen8 .own')).includes(value));
             let progGen8 = document.getElementById('progressBars').getElementsByTagName('progress')[7];
             progGen8.setAttribute("value", ownedGen8.length);
@@ -81,7 +81,7 @@ function countProgress(generation) {
             progGen8.setAttribute("max",   elemsGen8.length);
             break;
         case 'Gen9':
-            let elemsGen9 = document.querySelectorAll('.Gen9 > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsGen9 = document.querySelectorAll('.Gen9 > img:not([src="Images/Pokémon/0.png"])');
             let ownedGen9 = Array.from(elemsGen9).filter(value => Array.from(document.querySelectorAll('.Gen9 .own')).includes(value));
             let progGen9 = document.getElementById('progressBars').getElementsByTagName('progress')[8];
             progGen9.setAttribute("value", ownedGen9.length);
@@ -89,7 +89,7 @@ function countProgress(generation) {
             progGen9.setAttribute("max",   elemsGen9.length);
             break;
         case 'Novelty':
-            let elemsNovelty = document.querySelectorAll('.Novelty > img:not([src="https://i.imgur.com/tifFoSb.png"])');
+            let elemsNovelty = document.querySelectorAll('.Novelty > img:not([src="Images/Pokémon/0.png"])');
             let ownedNovelty = Array.from(elemsNovelty).filter(value => Array.from(document.querySelectorAll('.Novelty .own')).includes(value));
             let progNovelty = document.getElementById('progressBars').getElementsByTagName('progress')[9];
             progNovelty.setAttribute("value", ownedNovelty.length);
@@ -130,7 +130,7 @@ function mark(elem, generation, state = "own", count_now = true) {
 }
 
 function markAll() {
-    const elems = document.getElementById("mark").querySelectorAll('img:not([src="https://i.imgur.com/tifFoSb.png"])');
+    const elems = document.getElementById("mark").querySelectorAll('img:not([src="Images/Pokémon/0.png"])');
     /* an extra class on each img would be necessary if there were any img elements
     that were children of the single <main> element and not Pokémon, but there are not.
     Also, this means we don't attach event listeners to placeholders, so they do nothing
